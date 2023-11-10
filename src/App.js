3d1a836242a2c15af5eb454bc2af8ed2193a7461
thejunghare <prasadjunghare2@gmail.com>
2023-11-10 23:59:31 +0530

    add code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
+import TodoList from './TodoList'
+import { v4 as uuidv4 } from 'uuid'
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+// use state returns a array
 function App() {
+  const [todos, setTodos] = useState([]) // object destructuring (meaning Js experssion which allows us to extract data from arrays, object and maps and set them to new different variables)
+  const todoNameRef = useRef()
+  useEffect(() =>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id:uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <TodoList/>
+    // <input type="text"/> // -> this throws a error as we are trying to return two component using single return and function can return only one function component
+    // instead use fragements
+
+    <>
+      <TodoList todo={todos} /> {/*  todo={todos} -> this is knows as props */}
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left</div>
+    </>
+  )
 }
 
-export default App;
+export default App
